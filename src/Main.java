af0ede6b93e31c857bd1d711dc415c1c843b5306
1kovich <temirbaevkajrat132@gmail.com>
2023-10-11 17:51:54 +0600

    cgsio

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,33 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello");
-        scan
+        System.out.println("Выберите студента 1 до 2 ");
+        Scanner s = new Scanner(System.in);
+        int b = s.nextInt();
+        Kairat(b);
+        azamat(b);
+    }
+
+    static int Kairat(int b) {
+        if (b == 1) {
+            System.out.println("Зовут : Кайрат  ");
+            System.out.println("Место положение  : Кочкор-ата  ");
+            System.out.println("Место положение  : Кочкор-ата  ");
+
+        }
+        return b;
+    }
+
+    static int azamat(int b) {
+        if (b == 2) {
+            System.out.println("Зовут : Azamat ");
+            System.out.println("Место положение  : Кочкор-ата  ");
+            System.out.println("Возраст  : 15 ");
+
+
+        }
+        return b;
     }
 }
